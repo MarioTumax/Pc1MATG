a2d5630d81d9e9247aab2cb0aafc1d6709f178d9
Mario <mariotumax@emilianisomascos.edu.gt>
2026-01-30 08:56:08 -0600

    Se agrego texto

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,7 +5,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hola mundo");
-        System.out.println("iniciando");
+        System.out.println("iniciando en Analisis de Sistemas");
         System.out.println("en crear");
         System.out.println("repositorio");
         System.out.println("usando GIT");
