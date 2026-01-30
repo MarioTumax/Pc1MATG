ba78a53abca2f0a35d33d371c3ef0cdd4ffc00cd
Mario <mariotumax@emilianisomascos.edu.gt>
2026-01-30 08:50:47 -0600

    Mi primer commit

---
+++ src/main/Main.java
@@ -0,0 +1,14 @@
+
+package main;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+        System.out.println("iniciando");
+        System.out.println("en crear");
+        System.out.println("repositorio");
+        System.out.println("usando GIT");
+    }
+    
+}
